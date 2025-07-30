91e75f05d1e51e58e2d281fb21e6d23bd90be45f
tigranpetrosan <tapetrosian@gmail.com>
2025-07-31 01:40:10 +0300

    changed nav menu, added collaboration page

--- static/js/header.js
+++ static/js/header.js
@@ -1,14 +1,21 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-const filials = document.getElementById("filials");
-const filialsList = document.getElementById("filials-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+  const submenuIds = ["filials", "services", "extra"];
 
-addEventListener("DOMContentLoaded", () => {
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     hamburger.classList.toggle("active");
-    filials.addEventListener("click", () => {
-      filialsList.classList.toggle("active");
-    });
+  });
+
+  submenuIds.forEach(id => {
+    const menuItem = document.getElementById(id);
+    const submenu = document.getElementById(id + "-list");
+    if (menuItem && submenu) {
+      menuItem.addEventListener("click", (e) => {
+        e.preventDefault(); // чтобы не переходить по ссылке, если она есть
+        submenu.classList.toggle("active");
+      });
+    }
   });
 });
