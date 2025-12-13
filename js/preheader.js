871150951bc5cd99295042c3ac99ce58dbd8f8ea
LindaDevl <LindaDevl@users.noreply.github.com>
2025-12-13 09:40:16 +0000

    deploy: 4cc110c1f1e5c05420bbc2ea031cd2ad16ad0b23

---
+++ js/preheader.js
@@ -0,0 +1,12 @@
+// const preheader = document.querySelector("preheader");
+// const header = document.querySelector("header");
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > lastScrollY) {
+//     preheader.style.top = "-100%";
+//   } else {
+//     preheader.style.top = "100%";
+//   }
+//   lastScrollY = window.scrollY;
+//   let lastScrollY = window.scrollY;
+// });
